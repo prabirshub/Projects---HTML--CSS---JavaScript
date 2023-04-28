f314299bae51d0978aa30f9e167765930a9ad798
Prabir Singh <prabirsinghdigital@gmail.com>
2023-04-29 00:38:51 +0530

    FAQ Boxes completed

---
+++ FAQ-Boxes/script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('.faq-toggle')
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    btn.parentNode.classList.toggle('active')
+  })
+})
